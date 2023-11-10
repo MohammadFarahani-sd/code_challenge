3be9086f951864af9e5947bab9c70e99d1f0e11b
Mohammad <m.farahani@nizek.com>
2023-11-11 01:49:38 +0330

    init integration test with test utilies

---
+++ Test/Mc2.CrudTest.TestUtilities/SerializationHelpers/NewtonsoftHttpContentJsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Mc2.CrudTest.TestUtilities.SerializationHelpers;
+
+public static class NewtonsoftHttpContentJsonExtensions
+{
+    public static async Task<T> ReadFromJsonAsync<T>(this HttpContent content, JsonSerializerSettings? settings = null, CancellationToken cancellationToken = default)
+    {
+        if (content == null)
+        {
+            throw new ArgumentNullException(nameof(content));
+        }
+
+        var json = await content.ReadAsStringAsync(cancellationToken);
+
+        return JsonConvert.DeserializeObject<T>(json, settings)!;
+    }
+}
