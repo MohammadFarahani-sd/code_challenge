38322ada93c632ff650388c54e1524e2632a7b4d
Mohammad <m.farahani@nizek.com>
2023-11-11 00:20:26 +0330

    the domain testing was completed

---
+++ Test/Mc2.CrudTest.UnitTest/Customers/CustomerTestBuilder.cs
@@ -0,0 +1,73 @@
+﻿using Mc2.CrudTest.Domain.CustomerAggregate;
+
+namespace Mc2.CrudTest.UnitTest.Customers;
+
+public class CustomerTestBuilder
+{
+    private string firstname;
+
+    private string lastname;
+
+    private DateOnly dateOfBirth;
+
+    private ulong phoneNumber;
+
+    private string email;
+
+    private string? bankAccountNumber;
+
+
+    public CustomerTestBuilder()
+    {
+        var currentId = Guid.NewGuid();
+        firstname=Guid.NewGuid().ToString();
+        lastname=Guid.NewGuid().ToString();
+        dateOfBirth=DateOnly.FromDateTime(DateTime.Today);
+        phoneNumber =ulong.Parse("00989128986248");
+        email = $"{Guid.NewGuid().ToString()}@example.com";
+        bankAccountNumber = "123456789";
+        WithId(currentId);
+        
+    }
+
+    public Guid Id { get; set; }
+
+
+    internal CustomerTestBuilder WithId(Guid id)
+    {
+        Id = id;
+
+        return this;
+    }
+
+    internal CustomerTestBuilder WithFirstname(string firstname)
+    {
+      this.firstname = firstname;
+
+        return this;
+    }
+
+
+    internal CustomerTestBuilder WithLastname(string lastname)
+    {
+        this.lastname = lastname;
+
+        return this;
+    }
+
+
+    internal CustomerTestBuilder WithEmail(string email)
+    {
+        this.email = email;
+
+        return this;
+    }
+
+
+    public Customer CreateEntity()
+    {
+        var entity = new Customer(firstname, lastname,dateOfBirth, phoneNumber,email, bankAccountNumber);
+        return entity;
+    }
+    
+}
---
+++ Test/Mc2.CrudTest.UnitTest/Customers/CustomerTests.cs
@@ -0,0 +1,105 @@
+﻿using Mc2.CrudTest.Domain.CustomerAggregate;
+using Mc2.CrudTest.Domain.Exceptions;
+using Xunit;
+
+namespace Mc2.CrudTest.UnitTest.Customers;
+
+public class CustomerTests
+{
+    [Fact]
+    public void The_customer_created_properly_when_all_business_rules_are_valid()
+    {
+        //Arrange
+        CustomerTestBuilder builder = new CustomerTestBuilder();
+
+        //Act
+        Customer entity = builder.CreateEntity();
+
+        //Assert
+        Assert.NotNull(entity);
+
+        Assert.NotEqual(default, entity.Id);
+    }
+
+
+    [Fact]
+    public void customer_creating_properly_when_firstname_is_invalid()
+    {
+        //Arrange
+        CustomerTestBuilder? builder = new CustomerTestBuilder();
+
+        builder.WithFirstname(string.Empty);
+
+        //Act
+        Customer Action()
+        {
+            return builder.CreateEntity();
+        }
+
+        DomainException ex = Assert.Throws<DomainException>((Func<Customer>)Action);
+
+        //Assert            
+        Assert.NotNull(ex);
+        Assert.IsType<DomainException>(ex);
+    }
+
+
+    [Fact]
+    public void email_should_be_equal_with_expected_value()
+    {
+        //Arrange
+        var builder = new CustomerTestBuilder();
+        var email = "mfarahan8575@gmail.com";
+        builder.WithEmail(email);
+
+
+        //Act
+        Customer Action() => builder.CreateEntity();
+
+        // Assert
+        Assert.NotEmpty(Action().GetEmail().ToString());
+        Assert.Equal(email, Action().GetEmail().ToString());
+        Assert.NotEqual(email, Action().GetFirstName());
+    }
+
+    [Fact]
+    public void firstname_should_be_equal_with_expected_value()
+    {
+        //Arrange
+        var builder = new CustomerTestBuilder();
+        var firstname = "mohammad";
+        builder.WithFirstname(firstname);
+
+
+        //Act
+        Customer Action() => builder.CreateEntity();
+
+        // Assert
+        Assert.Equal(firstname, Action().GetFirstName());
+
+    }
+
+
+    [Fact]
+    public void not_valid_email_must_be_exception()
+    {
+        //Arrange
+        var builder = new CustomerTestBuilder();
+        var email = "not valid email";
+        builder.WithEmail(email);
+
+
+        //Act
+        Customer Action()
+        {
+            return builder.CreateEntity();
+        }
+
+        DomainException ex = Assert.Throws<DomainException>((Func<Customer>)Action);
+
+        //Assert            
+        Assert.NotNull(ex);
+        Assert.IsType<DomainException>(ex);
+    }
+
+}
