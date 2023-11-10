46105e71066208f706df41cde0e31c9bcc904f1e
Mohammad <m.farahani@nizek.com>
2023-11-11 00:38:52 +0330

    the first step of integration test was completed

---
+++ Mc2.CrudTest.Presentation/Server/Controllers/Models/CreateCustomerRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers.Models
+{
+    public class CreateCustomerRequest
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(128)]
+        public string Firstname { get; set; } = null!;
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(128)]
+        public string Lastname { get; set; } = null!;
+
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(128)]
+        public string Email { get; set; } = null!;
+
+        public DateOnly DateOfBirth { get; set; }
+        
+        public string? BankAccountNumber { get; set; }
+    }
+}
---
+++ Test/Mc2.CrudTest.IntegrationTest/Fixtures/Constants/CustomerConstants.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+namespace Mc2.CrudTest.IntegrationTest.Fixtures.Constants;
+
+public static class CustomerConstants
+{
+    
+    public const string Firstname = "SampleFirstname";
+    
+    public const string Lastname = "SampleLastname";
+    
+    public static readonly MailAddress Email =new MailAddress("mohammad@codechalleng.com");
+
+    public static DateOnly DateOfBirth = DateOnly.FromDateTime(DateTime.Today);
+
+    public const string BackAccountNumber = "123456789";
+
+    public static readonly Guid Id = new Guid("c1a0a567-6d1f-4864-97fd-abb23023c4fa");
+
+}
