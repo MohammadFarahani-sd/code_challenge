b81a9855f6ae0f40c675c4aa73bbb590519fcfbe
Mohammad <m.farahani@nizek.com>
2023-11-13 08:51:17 +0330

    project for query completed in the first step

---
+++ Query/Mc2.CrudTest.Query/EntityConfigurations/EntityTypeConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Mc2.CrudTest.Domain.SeedWork;
+using Mc2.CrudTest.Query.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Mc2.CrudTest.Query.EntityConfigurations;
+
+public abstract class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Entity
+{
+    private static readonly JsonSerializer Serializer = new()
+    {
+        DefaultValueHandling = DefaultValueHandling.Ignore,
+        ContractResolver = new DefaultContractResolver()
+    };
+
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.Property(x => x.Id);
+
+        ConfigureDerived(builder);
+    }
+
+    public abstract void ConfigureDerived(EntityTypeBuilder<TEntity> builder);
+
+    internal static string Serialize<T>(T obj)
+    {
+        using (StringWriter writer = new StringWriter())
+        {
+            Serializer.Serialize(writer, obj);
+            return writer.ToString();
+        }
+    }
+
+    internal static T Deserialize<T>(string json)
+    {
+        using (StringReader reader = new StringReader(json))
+        using (JsonTextReader jsonReader = new JsonTextReader(reader))
+        {
+            return Serializer.Deserialize<T>(jsonReader)!;
+        }
+    }
+}
---
+++ Query/Mc2.CrudTest.Query/Models/Customers/Mapping/Mapper.cs
@@ -0,0 +1,12 @@
+﻿using Mc2.CrudTest.Domain.CustomerAggregate;
+using Mc2.CrudTest.Query.Models.Customers.Response;
+
+namespace Mc2.CrudTest.Query.Models.Customers.Mapping;
+
+public static class Mapper
+{
+    public static CustomerResponse Map(Customer customer)
+    {
+        return CustomerResponse.Build(customer);
+    }
+}
