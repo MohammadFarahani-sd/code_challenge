78a68c3287b6cd5e663f0fe430cbd30fd0ac8986
Mohammad <m.farahani@nizek.com>
2023-11-13 19:01:09 +0330

    cleanup

--- Application/Mc2.CrudTest.Application/Command/Customers/CreateCustomerCommandHandler.cs
+++ Application/Mc2.CrudTest.Application/Command/Customers/CreateCustomerCommandHandler.cs
@@ -16,13 +16,11 @@ public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerComman
 
     public async Task<Guid> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
     {
-
         var isDuplicatedEmail=
             await _repository.IsUniqueEmail(request.Email);
         
         if (!isDuplicatedEmail)
             throw new DomainException("customer with this email is exist");
-
 
         var isDuplicatedData =
             await _repository.IsUniqueValidationPassed(request.Firstname, request.Lastname, request.DateOfBirth);
